2ab2392609aac6a9503cc8264d36b7ae3491533c
Mulverine Gaming <mulverin3@gmail.com>
2021-01-06 13:15:31 -0700

    Initial Commit

---
+++ sandstone.config.ts
@@ -0,0 +1,10 @@
+import type { SandstoneConfig } from 'sandstone/core'
+
+export default {
+  name: 'label',
+  description: [ 'A ', { text: 'Sandstone', color: 'gold' }, ' data pack.' ],
+  formatVersion: 6,
+  namespace: 'label',
+  packUid: 'KzCE6oTt',
+  saveOptions: { path: './.pack' }
+} as SandstoneConfig
