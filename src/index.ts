9f31904bfe600cfc2d1774ed61e69ca2206b4b60
Mulverine Gaming <mulverin3@gmail.com>
2021-01-06 15:24:58 -0700

    Should be good to go once ZZ fixes a bug

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,142 @@
-import { execute, tag } from "sandstone/commands";
-import { Selector } from "sandstone/variables";
+import { tag } from "sandstone/commands";
+import { Selector, SelectorClass } from "sandstone/variables";
+import { _ } from "sandstone/_internals";
 
-const namespace = { full: 'wasd', short: 'wasd' };
+import { ConditionType } from "sandstone/_internals/flow/conditions";
+import { SelectorProperties } from "sandstone/_internals/variables/selector";
 
 /**
  * Label tag (/tag) handler
  */
-export class Label {
+export class LabelClass {
   /**
-   * Label name
+   * Label Tag name
    */
   public name;
 
   /**
-   * Full Tag Name
+   * Label Description (optional)
    */
-  public raw_name;
+  public description: string | boolean = false;
+
+  public LabelHolder = (entity: SelectorClass<true> | '@s' | '@p' | '@r') => {
+    if (typeof entity === 'string') 
+      return new EntityLabel(Selector(entity) as SelectorClass<true>, this);
+    else
+      return new EntityLabel(entity, this);
+  }
 
   /**
-   * Selector to test for label, ie. `execute.as(foo.test())`
+   * Contains the name and description of the Label (eg. 'wasd:is_walking; Whether the player is not mounted')
    */
-  public test;
+  public toString = () => `${this.name}${this.description ? `; ${this.description}` : ''}`
 
-  constructor (name: string) {
-    this.name = name;
-    this.raw_name = `${namespace.full}.${name}`;
+  constructor (name: string, description: string | false) {
+    this.name = name
 
-    this.test = Selector('@s', { tag: this.raw_name });
+    if (description) this.description = description;
+  }
+}
+
+export class EntityLabel {
+  public label: LabelClass;
+
+  public selector: SelectorClass<true>;
+  
+  public originalSelector: SelectorClass<true>;
+
+  /**
+   * Test for label on entity
+   */
+  public _toMinecraftCondition() {
+    return { value: ['if', 'entity', this.selector.toString()] as any[] };
+  };
+
+  /** Test for label on entity */
+  public test = this as ConditionType;
+
+  /**
+   * Add label to entity
+   */
+  public add = () => tag(this.originalSelector).add(this.label.name);
+
+  /**
+   * Set label on/off for entity
+   */
+  public set = (set: boolean | ConditionType) => {
+    if (typeof set === 'boolean') {
+      if (set) tag(this.originalSelector).add(this.label.name);
+
+      else tag(this.originalSelector).remove(this.label.name);
+    } else {
+      _.if(set as ConditionType, () => tag(this.originalSelector).add(this.label.name))
+
+      .else(() => tag(this.originalSelector).remove(this.label.name));
+    }
+  };
+
+  /**
+   * Remove label from entity
+   */
+  public remove = () => tag(this.originalSelector).remove(this.label.name);
+
+  /**
+   * Contains the selector, and the name/description of the Label (eg. 'Whether @s has the label wasd:is_walking; Whether the player is not mounted')
+   */
+  public toString = () => `Whether ${this.originalSelector.toString()} has the label ${this.label.toString()}`
+
+  constructor (entity: SelectorClass<true>, label: LabelClass) {
+    this.originalSelector = entity;
+    this.label = label;
+
+    if (entity.arguments) {
+      if (entity.arguments.tag) {
+        if (typeof entity.arguments.tag === 'string')
+          entity.arguments.tag = [ entity.arguments.tag, label.name ];
+        else
+          entity.arguments.tag = [ ...entity.arguments.tag, label.name ];
+      }
+      else entity.arguments.tag = label.name;
+    }
+    else entity.arguments = { tag: label.name } as SelectorProperties<true, false>;
+
+    this.selector = entity;
   }
 }
 
 /**
  * Creates a new label
  * @param label Label/tag name
+ * @param description Label description (optional)
  */
-export function newLabel(label: string) {
-  return new Label(label);
+export function newLabel(label: string, description: string | false = false) {
+  return new LabelClass(label, description);
 }
 
 /**
  * Adds label to `@s`
  * @param label Label
  */
-export function addLabel(label: Label) {
-  tag('@s').add(label.raw_name)
-}
+export function addLabel (label: LabelClass) {
+  const target = label.LabelHolder('@s');
+  target.add();
+  return target;
+};
 
 /**
  * Removes label from `@s`
  * @param label Label
  */
-export function removeLabel(label: Label) {
-  tag('@s').remove(label.raw_name)
+export function removeLabel(label: LabelClass) {
+  const target = label.LabelHolder('@s');
+  target.remove();
+  return target;
 }
 
 /**
- * Removes label from `@s`
+ * Test for label on `@s`
  * @param label Label
- * @param run Function to run
  */
-export function hasLabel(label: Label, run: () => void) {
-  execute.as(label.test).run(run);
+export function hasLabel(label: LabelClass) {
+  return label.LabelHolder('@s').test
 }
